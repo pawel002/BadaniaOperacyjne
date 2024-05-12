6bf752b9b3839a853e0f0fb74e088f938fae31c0
pawel002 <pawel.gandalf@gmail.com>
2024-05-13 00:34:09 +0200

    added genetic loop, one mutation, one crossover

--- relaxed.py
+++ relaxed.py
@@ -3,6 +3,19 @@ import copy
 from classes import *
 import matplotlib.pyplot as plt
 
+class RelaxedInstance:
+    def __init__(self, task_distrib) -> None:
+        self.task_distrib = task_distrib
+
+    def evaluate(self, tasks: list[Task]):
+        profit = 0
+        for row in self.task_distrib:
+            for task_id in row:
+                profit += tasks[task_id].profit
+
+        return profit
+        
+
 def generateTasks(count: int, work_dealine: int, maxprofit: int | float) -> list[Task]:
     tasks = []
     for _ in range(count):
@@ -13,36 +26,176 @@ def generateTasks(count: int, work_dealine: int, maxprofit: int | float) -> list
     return tasks
 
 def generateRandomSolution(tasks: list[Task], worker_count: int):
-    dealine = tasks[0].deadline # same for every task
+    deadline = tasks[0].deadline # same for every task
     task_distrib = [[] for _ in range(worker_count)] # t_d[i] = tasks for i-th worker
     worker_times = [0 for _ in range(worker_count)]
     unused_tasks = [i for i in range(len(tasks))]
     profit = 0
 
-    for task_id, task in enumerate(tasks):
+    gen = list(enumerate(tasks))
+    random.shuffle(gen)
+
+    for task_id, task in gen:
         for i in range(worker_count):
-            if worker_times[i] + task.time <= dealine:
+            if worker_times[i] + task.time <= deadline:
                 worker_times[i] += task.time
                 task_distrib[i].append(task_id)
                 profit += task.profit
                 unused_tasks.remove(task_id)
                 break
 
-    for i in range(worker_count):
-        print(f"time = {worker_times[i]}")
-        print(*task_distrib[i])
+        if sum(worker_times) == worker_count * deadline:
+            break
 
     return task_distrib, profit, unused_tasks
 
-def evolutionAlg1(population,  tasks):
-    # implement lol
-    pass
+def genetic_func(instance1: RelaxedInstance, instance2: RelaxedInstance, tasks: list[Task]):
 
+    worker_count = len(instance1.task_distrib)
+    deadline = tasks[0].deadline
+    worker_perm = [i for i in range(worker_count)]
+
+    def fit(time, instance, task_idx):
+
+        for worker_idx in worker_perm:
+
+            if time[worker_idx] + tasks[task_idx].time <= deadline:
+                time[worker_idx] += tasks[task_idx].time
+                instance[worker_idx].append(task_idx)
+                return True
+            
+        return False
+
+    used1 = [i for row in instance1.task_distrib for i in row]
+    used2 = [i for row in instance2.task_distrib for i in row]
+
+    mark_used = [0 for _ in range(len(tasks))]
+
+    new_instance1 = [[] for _ in range(worker_count)]
+    worker_time1 = [0 for i in range(worker_count)]
+    
+    new_instance2 = [[] for _ in range(worker_count)]
+    worker_time2 = [0 for i in range(worker_count)]
+
+    for task_set in [used1, used2]:
+        for task_idx in task_set:
+
+            random.shuffle(worker_perm)
+            if mark_used[task_idx] == 1:
+
+                fit(worker_time2, new_instance2, task_idx)
+                continue
+
+            if mark_used[task_idx] == 2:
+
+                fit(worker_time1, new_instance1, task_idx)
+                continue
+
+            if random.random() < 0.5:
+                
+                if fit(worker_time1, new_instance1, task_idx):
+                    continue
+                
+                fit(worker_time2, new_instance2, task_idx)
+
+            else:
+                
+                if fit(worker_time2, new_instance2, task_idx):
+                    continue
+
+                fit(worker_time1, new_instance1, task_idx)
+
+    return RelaxedInstance(new_instance1), RelaxedInstance(new_instance2)
+
+def mutate(instance: RelaxedInstance, tasks: list[Task], pop_prob: float):
+
+    worker_count = len(instance.task_distrib)
+    deadline = tasks[0].deadline
+    worker_perm = [i for i in range(worker_count)]
+    new_instance = [[x for x in row if random.random() < pop_prob] for row in instance.task_distrib]
+    worker_time = [sum([tasks[i].profit for i in row]) for row in new_instance]
+
+    def fit(time, instance, task_idx):
+
+        for worker_idx in worker_perm:
+
+            if time[worker_idx] + tasks[task_idx].time <= deadline:
+                time[worker_idx] += tasks[task_idx].time
+                instance[worker_idx].append(task_idx)
+                return True
+            
+        return False
+
+    used = set(i for row in new_instance for i in row)
+    unused = set(range(len(tasks))) - used
+    unused_profit = [(tasks[i].profit, i) for i in unused]
+    unused_profit.sort(reverse=True)
+
+    for _, task_id in unused_profit:
+
+        fit(worker_time, new_instance, task_id)
+
+    return RelaxedInstance(new_instance)
+
+def evolutionAlg(population: list[RelaxedInstance], tasks: list[Task], iterations: int, mutation_prob: float):
+    
+    population_size = len(population)
+    population.sort(key=lambda x: x.evaluate(tasks), reverse=True)
+    best_instance, best_profit = None, 0
+    average, best, pop_best, pop_worst = [], [], [], []
+
+    for iter_count in range(iterations):
+
+        for population_idx in range(0, len(population), 2):
+            
+            i1, i2 = population[population_idx], population[population_idx + 1]
+            new_i1, new_i2 = genetic_func(i1, i2, tasks)
+
+            if random.random() < mutation_prob:
+                population.append(mutate(new_i1, tasks, 0.25))
+
+            if random.random() < mutation_prob:
+                population.append(mutate(new_i2, tasks, 0.25))
+   
+            population.append(new_i1)
+            population.append(new_i2)
+
+        population.sort(key=lambda x: x.evaluate(tasks), reverse=True)
+        population = population[:population_size]
+
+        population_evaluation = np.average(np.array([x.evaluate(tasks) for x in population]))
+        cur_best, cur_best_ev = population[0], population[0].evaluate(tasks)
+
+        if cur_best_ev > best_profit:
+            best_profit = cur_best_ev
+            best_instance = cur_best
+
+        average.append(population_evaluation)
+        best.append(best_profit)
+        pop_best.append(cur_best_ev)
+        pop_worst.append(population[-1].evaluate(tasks))
+
+        print(f"{iter_count} ITERATION. \n      average = {population_evaluation:.2f} \n      curbest = {cur_best_ev:.2f} \n      alltime = {best_profit:.2f}")
+
+        if population_evaluation >= best_profit * 0.99999:
+            break
+
+    x_values = [i for i in range(len(best))]
+    plt.plot(x_values, average, label="Average population profit")
+    plt.plot(x_values, best, label="Best Profit")
+    plt.scatter(x_values, pop_best, label="Best Profit For Population", s=0.1)
+    plt.scatter(x_values, pop_worst, label="Worst Profit For Population", s=0.1)
+    plt.xlabel("Iteration")
+    plt.ylabel("Profit Score")
+    plt.legend()
+    plt.show()
+
+    return best_instance
 
 def evolution(population, tasks, deadline):
     
     # remove random parts from solution
-    old_population = copy.deepcopy(population)
+    population = copy.deepcopy(population)
     for solution_idx, (task_distribution, profit, unassigned_tasks) in enumerate(population):
         distribution_copy = copy.deepcopy(task_distribution)
         unassigned_copy = copy.deepcopy(unassigned_tasks)
@@ -93,53 +246,62 @@ def calculate_profit_from_distribution(task_distribution, tasks):
 if __name__ == "__main__":
     random.seed(42)
     np.random.seed(42)
-    BASE_POPULATION_SIZE = 10
-    WORKER_COUNT = 3
-    DEADLINE = 10
-    MAX_ITER = 500
+    BASE_POPULATION_SIZE = 40
+    WORKER_COUNT = 10
+    DEADLINE = 50
+    TASK_COUNT = 500
+    MAX_ITER = 200
 
-    DEBUG_LOG = True 
         
-    tasks = generateTasks(20, DEADLINE, 10)
-    # task_distribution, profit = generateRandomSolution(tasks, WORKER_COUNT)
-    base_population = []
+    # tasks = generateTasks(TASK_COUNT, DEADLINE, 10)
+    # # # task_distribution, profit = generateRandomSolution(tasks, WORKER_COUNT)
+    # base_population = []
+    # for _ in range(BASE_POPULATION_SIZE):
+    #     base_population.append(generateRandomSolution(tasks, WORKER_COUNT))
+
+    # print(*base_population)
+    # print('-'*15)
+
+    # best_profit = 0
+    # best_distribution = base_population[0]
+    # profit_list = []
+    # best_profit_list = []
+
+    # for iter in range(MAX_ITER):
+    #     current_population = evolution(base_population, tasks, DEADLINE)
+
+    #     for current_solution in current_population:
+    #         current_profit = calculate_profit_from_distribution(current_solution[0], tasks)
+
+    #         if current_profit > best_profit:
+    #             best_profit = current_profit
+    #             best_distribution = current_solution[0]
+
+
+    #         print(f"Current best profit:\n{best_profit}")
+    #         if DEBUG_LOG:
+    #             print('-'*20)
+    #             print(f"Current iter:\n{iter}")
+    #             print(f"Current profit:\n {current_profit}")
+    #             # print(f"Population: {current_population}")
+
+    #     profit_list.append(current_profit)
+    #     best_profit_list.append(best_profit)
+
+    # x_values = [i for i in range(len(profit_list))]
+    # plt.plot(x_values, profit_list, label="Current Profit")
+    # plt.plot(x_values, best_profit_list, label="Current Best Profit")
+    # plt.xlabel("Iteration")
+    # plt.ylabel("Profit Score")
+    # plt.show()
+    # print(*best_distribution)
+
+    tasks = generateTasks(TASK_COUNT, DEADLINE, 10)
+
+    population = []
     for _ in range(BASE_POPULATION_SIZE):
-        base_population.append(generateRandomSolution(tasks, WORKER_COUNT))
+        task_distrib, _, _ = generateRandomSolution(tasks, WORKER_COUNT)
+        population.append(RelaxedInstance(task_distrib))
 
-    print(*base_population)
-    print('-'*15)
-
-    best_profit = 0
-    best_distribution = base_population[0]
-    profit_list = []
-    best_profit_list = []
-
-    for iter in range(MAX_ITER):
-        current_population = evolution(base_population, tasks, DEADLINE)
-
-        for current_solution in current_population:
-            current_profit = calculate_profit_from_distribution(current_solution[0], tasks)
-
-            if current_profit > best_profit:
-                best_profit = current_profit
-                best_distribution = current_solution[0]
-
-
-            print(f"Current best profit:\n{best_profit}")
-            if DEBUG_LOG:
-                print('-'*20)
-                print(f"Current iter:\n{iter}")
-                print(f"Current profit:\n {current_profit}")
-                # print(f"Population: {current_population}")
-
-        profit_list.append(current_profit)
-        best_profit_list.append(best_profit)
-
-    x_values = [i for i in range(len(profit_list))]
-    plt.plot(x_values, profit_list, label="Current Profit")
-    plt.plot(x_values, best_profit_list, label="Current Best Profit")
-    plt.xlabel("Iteration")
-    plt.ylabel("Profit Score")
-    plt.show()
-    print(*best_distribution)
+    best = evolutionAlg(population, tasks, MAX_ITER, 0.2)
 
